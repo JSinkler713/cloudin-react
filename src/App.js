c0b12a769f3bfde2dfe43474d603b5d6acbef8fe
James Sinkler <jamessinkler713@gmail.com>
2021-03-12 07:55:05 -0800

    includes example with fetch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   const [photo, setPhoto] = useState('')
   const [url, setUrl] = useState('')
+
   const handleSubmit = (e)=> {
     e.preventDefault()
     console.log('submit')
@@ -23,9 +24,34 @@ function App() {
         .catch(err => console.log(err))
     };
   
+  const handleSubmitFetch = (e)=> {
+    e.preventDefault()
+    console.log('submit')
+    let form_data = new FormData();
+    form_data.append('image', photo, 'image');
+    const options = {
+      method: 'POST',
+      body: form_data,
+      // Counterintuitively don't use this with fetch
+      // boundary not found with this header 
+      /*
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      }
+      */
+    }
+    fetch('http://localhost:8000/images/multer', options)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          setUrl(data.url)
+        })
+        .catch(err => console.log(err))
+    };
+
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmitFetch}>
         <input type='file' onChange={(event)=> {
           setPhoto(event.target.files[0])
         }} />
